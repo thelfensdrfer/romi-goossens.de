b61d7f8120d0e0d6770d907cb91e9498511515d6
Tim Helfensdörfer <thelfensdrfer@gmail.com>
2022-02-10 00:59:08 +0100

    feat: initial commit

---
+++ src/README.md
@@ -0,0 +1,8 @@
+## Emma Gooßens
+
+Navigate into your new site’s directory and start it up.
+
+ ```shell
+ cd my-gatsby-site/
+ npm run develop
+ ```
---
+++ src/src/pages/index.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+const IndexPage = () => {
+    return (
+        <div className="h-screen p-8">
+            <div className="flex flex-col h-full">
+                <main className="flex-1">
+                    <title>Emma Gooßens</title>
+                    <h1 className="text-9xl text-center mt-4 underline decoration-pink-500">Emma Gooßens</h1>
+                </main>
+
+                <footer className="text-gray-500 text-right">
+                    <p>
+                        <a target="_blank" href="https://thelfensdrfer.de/impressum.html">Impressum</a>&nbsp;|&nbsp;
+                        <a target="_blank" href="https://thelfensdrfer.de/datenschutz.html">Datenschutzerklärung</a></p>
+                </footer>
+            </div>
+        </div>
+    )
+}
+
+export default IndexPage
